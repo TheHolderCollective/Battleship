e45581119389f1bab09dd24c81c4d276b33055c0
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2025-08-29 21:39:07 -0700

    Update WindowHeightScaleFactor

--- BattleshipGame/Properties/Resources.Designer.cs
+++ BattleshipGame/Properties/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace BattleshipGame.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.35.
+        ///   Looks up a localized string similar to 1.34.
         /// </summary>
         internal static string WindowHeightScaleFactor {
             get {
